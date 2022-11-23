047361aa150903d9fbfda1387e9f92a8235be357
nkr22 <81866898+nkr22@users.noreply.github.com>
2022-11-23 16:03:07 -0700

    pretty styles and slug attempt

--- donut.js
+++ donut.js
@@ -1,10 +1,19 @@
 function get_donut() {
-    let paramString = window.location.search;
-    let queryString = new URLSearchParams(paramString);
+  //   let paramString = window.location.search;
+  // let queryString = new URLSearchParams(paramString);
 
-    for (let pair of queryString.entries()) {
-    console.log("Key is: " + pair[0]);
-    console.log("Value is: " + pair[1]);
-    }
+  // for (let pair of queryString.entries()) {
+  // console.log("Key is: " + pair[0]);
+  // console.log("Value is: " + pair[1]);
+  // }
+  //   var donut = paramSting;
 
+  const urlParams = new URLSearchParams(window.location.search);
+  const donutValue = urlParams.get("donut");
+  console.log(donut);
+
+  APIUrl = "https://ck8vh2jy5l.execute-api.us-east-1.amazonaws.com";
+  fetch(APIUrl + "/items" + donut)
+    .then((response) => response.json())
+    .then((data) => parseDonutAPIData(data));
 }
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-
 function showDonutItems() {
-    APIUrl = "https://ck8vh2jy5l.execute-api.us-east-1.amazonaws.com";
-    fetch(APIUrl + "/items")
+  APIUrl = "https://ck8vh2jy5l.execute-api.us-east-1.amazonaws.com";
+  fetch(APIUrl + "/items")
     .then((response) => response.json())
     .then((data) => parseDonutAPIData(data));
 }
@@ -9,15 +8,16 @@ function parseDonutAPIData(data) {
   // An array of Items is returned, loop through them and print their values
   if (data.Items.length > 0) {
     for (let iCount = 0; iCount < data.Items.length; iCount++) {
-      var src = (data.Items[iCount].URL);
-      var name = (data.Items[iCount].Name);
-      var pk = (data.Items[iCount].pk);
-      var sk = (data.Items[iCount].sk);
-      var link = (data.Items[iCount].pk + "!" + data.Items[iCount].sk);
-      document.getElementById("donut_list").innerHTML +=
-        `<div class="col-3 card">
+      var src = data.Items[iCount].URL;
+      var name = data.Items[iCount].Name;
+      var pk = data.Items[iCount].pk;
+      var sk = data.Items[iCount].sk;
+      var link = data.Items[iCount].pk + "!" + data.Items[iCount].sk;
+      document.getElementById(
+        "donut_list"
+      ).innerHTML += `<div class="col-3 card">
+        <a href=donut.html?donut=${link}>
             <img src=${src} />
-            <a href=/donut.html?donut=${link}>
               <h2>${name}</h1>
             </a>
         </div> `;
