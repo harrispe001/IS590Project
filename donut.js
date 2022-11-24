a73cdb9ffc545bfe2086f6c618e62c7c2b41f744
nkr22 <81866898+nkr22@users.noreply.github.com>
2022-11-23 18:53:45 -0700

    individual page and attempt at active navbar

--- donut.js
+++ donut.js
@@ -1,19 +1,43 @@
 function get_donut() {
-  //   let paramString = window.location.search;
-  // let queryString = new URLSearchParams(paramString);
+    //   let paramString = window.location.search;
+    // let queryString = new URLSearchParams(paramString);
 
-  // for (let pair of queryString.entries()) {
-  // console.log("Key is: " + pair[0]);
-  // console.log("Value is: " + pair[1]);
-  // }
-  //   var donut = paramSting;
+    // for (let pair of queryString.entries()) {
+    // console.log("Key is: " + pair[0]);
+    // console.log("Value is: " + pair[1]);
+    // }
+    //   var donut = paramSting;
 
-  const urlParams = new URLSearchParams(window.location.search);
-  const donutValue = urlParams.get("donut");
-  console.log(donut);
+    const urlParams = new URLSearchParams(window.location.search);
+    const donutValue = urlParams.get("donut");
+    console.log(donutValue);
 
-  APIUrl = "https://ck8vh2jy5l.execute-api.us-east-1.amazonaws.com";
-  fetch(APIUrl + "/items" + donut)
-    .then((response) => response.json())
-    .then((data) => parseDonutAPIData(data));
+    APIUrl = "https://ck8vh2jy5l.execute-api.us-east-1.amazonaws.com";
+    fetch(APIUrl + "/items" + donutValue)
+        .then((response) => response.json())
+        .then((data) => parseOneDonut(data));
+}
+
+function parseOneDonut(data) {
+    // An array of Items is returned, loop through them and print their values
+    var src = data.Item.URL;
+    var name = data.Item.Name;
+    var pk = data.Item.pk;
+    var sk = data.Item.sk;
+    var link = data.Item.pk + "!" + data.Item.sk;
+    var price = data.Item.price;
+    var allergens = data.Item.allergens;
+    var quote = data.Item.quote;
+
+    document.getElementById(
+        "donut_list"
+    ).innerHTML += `<div class="col-6 text-center">
+      <img src=${src} />
+      <h2>${price}</h2>
+    </div> 
+    <div class="col-6 text-center">
+      <h2 class="title">${name}</h1>
+      <p>${quote}</p>
+      <a href="https://s3.amazonaws.com/kkd-e1-images.kktestkitchen.com/ecomm/nutrition/11010-nutrition.pdf" id="ctl00_plcMain_linkNutritionCta" class="btn btn-primary shadow cta-primary nutrition" data-track="item-detail-nutrition" target="_blank"><span>Nutritional &amp; Allergen Info</span></a>
+    </div>`;
 }
--- index.js
+++ index.js
@@ -1,26 +1,39 @@
 function showDonutItems() {
-  APIUrl = "https://ck8vh2jy5l.execute-api.us-east-1.amazonaws.com";
-  fetch(APIUrl + "/items")
-    .then((response) => response.json())
-    .then((data) => parseDonutAPIData(data));
+    APIUrl = "https://ck8vh2jy5l.execute-api.us-east-1.amazonaws.com";
+    fetch(APIUrl + "/items")
+        .then((response) => response.json())
+        .then((data) => parseDonutAPIData(data));
 }
 function parseDonutAPIData(data) {
-  // An array of Items is returned, loop through them and print their values
-  if (data.Items.length > 0) {
-    for (let iCount = 0; iCount < data.Items.length; iCount++) {
-      var src = data.Items[iCount].URL;
-      var name = data.Items[iCount].Name;
-      var pk = data.Items[iCount].pk;
-      var sk = data.Items[iCount].sk;
-      var link = data.Items[iCount].pk + "!" + data.Items[iCount].sk;
-      document.getElementById(
-        "donut_list"
-      ).innerHTML += `<div class="col-3 card">
-        <a href=donut.html?donut=${link}>
+    // An array of Items is returned, loop through them and print their values
+    if (data.Items.length > 0) {
+        for (let iCount = 0; iCount < data.Items.length; iCount++) {
+            var src = data.Items[iCount].URL;
+            var name = data.Items[iCount].Name;
+            var pk = data.Items[iCount].pk;
+            var sk = data.Items[iCount].sk;
+            var link =
+                "/" + data.Items[iCount].pk + "!" + data.Items[iCount].sk;
+            document.getElementById(
+                "donut_list"
+            ).innerHTML += `<div class="col-3 card">
+        <a href="donut.html?donut=${link}">
             <img src=${src} />
               <h2>${name}</h1>
             </a>
         </div> `;
+        }
     }
-  }
+}
+
+function active() {
+    var itemContainer = document.getElementById("navbar");
+    var items = itemContainer.getElementsByClassName("nav-item");
+    for (var i = 0; i < items.length; i++) {
+        items[i].addEventListener("load", function () {
+            var current = document.getElementsByClassName("active");
+            current[0].className = current[0].className.replace(" active", "");
+            this.className += " active";
+        });
+    }
 }
